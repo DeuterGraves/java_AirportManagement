b49238bd883adff068ad6988d660eff2b54d42f9
DeuterGraves <carolinetgraves@gmail.com>
2018-11-25 12:55:42 +0000

    Airport class and basic functionality added

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+
 public class Airport {
+
+    private String code;
+    private String motto;
+    private ArrayList<Hangar> hangars;
+    private ArrayList<Flight> flights;
+
+//    since building in theory for 1 specific airport, I am tempted to just hard code in these parameters
+    public Airport(String code, String motto){
+        this.code = code;
+        this.motto = motto;
+        this.hangars = new ArrayList<>();
+        this.flights = new ArrayList<>();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMotto() {
+        return motto;
+    }
+
+    public int hangarCount(){
+        return hangars.size();
+    }
+
+    public int flightCount(){
+        return flights.size();
+    }
+//
+//    public void addHangar(HangarType){
+//        hangars.add(HangarType);
+//    }
+
+    public void addFlight(Flight flight){
+        flights.add(flight);
+    }
 }
--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -1,2 +1,52 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class AirportTest {
+
+    private Plane plane;
+    private Hangar hangar;
+    private Flight flight;
+    private Airport airport;
+
+    @Before
+    public void before(){
+        plane = new Plane(ModelType.BOEING787, "Jamie Airways", "DJ763JF");
+        hangar = new Hangar(HangarType.A1);
+        flight = new Flight(plane, "JA-54", "Burmuda");
+        airport = new Airport("DCA", "NOT Regan National");
+    }
+
+    @Test
+    public void airportHasAirportCode(){
+        assertEquals("DCA", airport.getCode());
+    }
+
+    @Test
+    public void airportHasNoHangars(){
+        assertEquals(0, airport.hangarCount());
+    }
+//
+//    @Test
+//    public void airportCanAddHangars(){
+//        airport.addHangar(hangar);
+//        assertEquals(1, airport.hangarCount());
+//    }
+//
+    @Test
+    public void airportHasNoFlights(){
+        assertEquals(0, airport.flightCount());
+    }
+
+    @Test
+    public void airportCanAddFlights(){
+        airport.addFlight(flight);
+        assertEquals(1, airport.flightCount());
+    }
+
+    @Test
+    public void airportHasMotto(){
+        assertEquals("NOT Regan National", airport.getMotto());
+    }
 }
