dc9b3ba357cf05549064b7e56934416707a86ec4
DeuterGraves <carolinetgraves@gmail.com>
2018-11-25 22:35:29 +0000

    ticket class added and tested

--- src/main/java/Ticket.java
+++ src/main/java/Ticket.java
@@ -1,2 +1,38 @@
 public class Ticket {
+
+    private int ticketNumber;
+    private Passenger passenger;
+    private Flight flight;
+    private int price;
+
+    public Ticket(int ticketNumber, Passenger passenger, Flight flight, int price){
+        this.ticketNumber = ticketNumber;
+        this.passenger = passenger;
+        this.flight = flight;
+        this.price = price;
+    }
+
+    public int getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public String getPassengerName() {
+        return passenger.getfName() + " "+ passenger.getlName();
+    }
+
+    public String getFlightNumber() {
+        return flight.getFlightNumber();
+    }
+
+    public int getFlightCapacity(){
+        return flight.getCapacity();
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDestination(){
+        return flight.getDestination();
+    }
 }
--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -1,2 +1,50 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TicketTest {
+
+    private Passenger passenger;
+    private Plane plane;
+    private Flight flight;
+    private Ticket ticket;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("Maisie", "Graves");
+        plane = new Plane(ModelType.BOEING787, "British Airways", "GH2359P");
+        flight = new Flight(plane, "BA-32", "Okinawa");
+        ticket = new Ticket(001, passenger, flight, 350);
+    }
+
+    @Test
+    public void tickettHastTicketNumber(){
+        assertEquals(1, ticket.getTicketNumber());
+    }
+
+    @Test
+    public void ticketHasPassenger(){
+        assertEquals("Maisie Graves", ticket.getPassengerName());
+    }
+
+    @Test
+    public void ticketHasFlightNumber(){
+        assertEquals("BA-32", ticket.getFlightNumber());
+    }
+
+    @Test
+    public void ticketHasPrice(){
+        assertEquals(350, ticket.getPrice());
+    }
+
+    @Test
+    public void ticketHasDestination(){
+        assertEquals("Okinawa", ticket.getDestination());
+    }
+
+    @Test
+    public void ticketHasFlightCapacity(){
+        assertEquals(290, ticket.getFlightCapacity());
+    }
 }
